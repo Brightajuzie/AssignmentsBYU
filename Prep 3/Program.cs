1805bf68d15db6aaf897afca76711373dbfc9145
Bright Onyekachi <brightajuzie@gmail.com>
2024-09-28 12:51:02 +0100

    new

---
+++ Prep 3/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+ 
+ Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+        while (guess != magicNumber)
+        {
+            Console.WriteLine("What is your Guess");
+            guess= int.Parse(Console.ReadLine());
+           
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("This looks Bigger");
+            }
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("this looks Smaller");
+            }
+            else
+            {
+                Console.WriteLine("You got it");
+            }
+
+        }
---
+++ Prep 4/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Collections.Generic;
+List<int> numbers = new List<int>();
+
+int numbertouse = -1;
+while (numbertouse != 0)
+{
+    Console.Write("Enter a number  (PRESS 0 to quit):");
+    string response = Console.ReadLine();
+    numbertouse = int.Parse(response);
+
+    if (numbertouse!=0)
+    {
+        numbers.Add(numbertouse);
+    }
+}
+// Computing SUM
+int sum = 0;
+foreach (int number in numbers)
+{
+    sum+= number;
+}
+Console.WriteLine($" The Sum is {sum}");
+
+//Computing the Average
+float averge = ((float)sum)/ numbers.Count;
+Console.WriteLine($"The Average is {averge}");
+
+//Max
+int max = numbers[0];
+foreach(int number in numbers)
+{
+    if(number > max )
+    {
+        max = number;
+    }
+}
+Console.WriteLine($"The Max is {max}");
